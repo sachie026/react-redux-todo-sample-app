a8666e043735c1e279231a38fe3e5230315440ef
Sachin Jadhav <sachinjadhav@News-MacBook-Pro.local>
2019-11-24 16:25:46 +0530

    added react routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/list">List</Link>
+            </li>
+            <li>
+              <Link to="/add">Add</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path  = "/list">
+              List
+          </Route>
+
+          <Route path  = "/home">
+              Home
+          </Route>
+
+          <Route path  = "/add">
+              Add
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
