a4c5399c56a25e58157ee646afdada04b82cd0d1
Sachin Jadhav <jadhavsachin174@gmail.com>
2019-11-26 11:26:26 +0530

    added reducer code for add

---
+++ src/reducers/add.js
@@ -0,0 +1,13 @@
+const Add = (state = [], action) => {
+    switch(action.type){
+        case ("ADD"):
+        break;    
+        ;
+        default:
+            break;
+        ;
+    }
+    return null;
+}
+
+export default Add
