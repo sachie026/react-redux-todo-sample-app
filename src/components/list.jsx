4f020a2e56ef22128c253a97441a98adfd90d0fa
Sachin Jadhav <jadhavsachin174@gmail.com>
2019-11-27 13:40:53 +0530

    added entries are shown on the list page

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
+import {connect} from "react-redux"
 
-export default class List extends Component {
+class List extends Component {
     render() {
         return (    
             <div className= "container">
                 <ul className="todo-list-ul">
-                    <li>one</li>
-                    <li>two</li>
-                    <li>three</li>
-                    <li>four</li>
+
+            {
+            this.props.list.map((item) => <li key = {item.id}>{item.title}</li>)
+            }
                 </ul>
             </div>
         )
     }
 }
+
+
+const mapStateToProps = (state) => {
+    return {
+        list: state
+    }
+}
+
+export default connect(mapStateToProps)(List)
